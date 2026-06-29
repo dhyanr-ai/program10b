a09dce6f0e77e36b81a303ff41a4104149ac1c87
Dhyan R <dhyanr8055@gmail.com>
2026-06-29 10:29:52 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -2,12 +2,12 @@ a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 
 print("\nBefore swapping:")
-print("First number =", a)
-print("Second number =", b)
+print("First number = 5", a)
+print("Second number = 10", b)
 
 
 a, b = b, a
 
 print("\nAfter swapping:")
-print("First number =", a)
-print("Second number =", b)
+print("First number = 10", a)
+print("Second number = 5", b)
