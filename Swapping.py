d4c72dfea26b1cbf18158c1fcf24e1c62d83436c
Dhyan R <dhyanr8055@gmail.com>
2026-06-29 10:32:27 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -1,5 +1,5 @@
-a = input("Enter the first number:"))
-b = input("Enter the second number:"))
+a = input("Enter the first number:")
+b = input("Enter the second number:")
 
 print("\nBefore swapping:")
 print("First number =", a)
