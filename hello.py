36697085c91e75eab12b1c1612cc3dc1e34e8c84
Dhyan R <dhyanr8055@gmail.com>
2026-06-29 09:41:58 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("\nBefore swapping:")
+print("First number =", a)
+print("Second number =", b)
+
+
+a, b = b, a
+
+print("\nAfter swapping:")
+print("First number =", a)
+print("Second number =", b)
